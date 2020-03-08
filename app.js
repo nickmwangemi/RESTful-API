0c4d6285ff81c6c175c3dc8c3439b0a1dc86ccde
nickmwangemi <nickmwangemi@gmail.com>
2020-03-08 18:43:15 +0300

    Added error handling and improved project setup

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 // Import routes
 const productRoutes = require('./api/v1/routes/products');
 const orderRoutes = require('./api/v1/routes/orders');
 
+// Funnel all requests through morgan logger
+app.use(morgan('dev'));
+// Request handlers
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+	const error = new Error('Not Found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message,
+		},
+	});
+});
 
 module.exports = app;
