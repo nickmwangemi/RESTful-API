114bdc5876d5344411613c72f5ae4219fd65323a
nickmwangemi <nickmwangemi@gmail.com>
2020-03-08 22:18:53 +0300

    Added logic to parse request bodies and handle CORS errors

--- api/v1/routes/orders.js
+++ api/v1/routes/orders.js
@@ -10,8 +10,13 @@ router.get('/', (req, res, next) => {
 
 // POST an order
 router.post('/', (req, res, next) => {
+	const order = {
+		productId: req.body.productId,
+		quantity: req.body.quantity,
+	};
 	res.status(201).json({
 		message: 'An order was created',
+		createdOrder: order,
 	});
 });
 
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // Import routes
 const productRoutes = require('./api/v1/routes/products');
@@ -8,6 +9,23 @@ const orderRoutes = require('./api/v1/routes/orders');
 
 // Funnel all requests through morgan logger
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+
+// Handle CORS errors
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+	);
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Headers', 'PUT,POST,PATCH,DELETE,GET');
+		res.status(200).json({});
+	}
+	next();
+});
+
 // Request handlers
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
